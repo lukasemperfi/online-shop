96eb08b686fd9054a986c8bd087be2fa0b6756e5
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2022-06-05 23:29:28 +0300

    prettier

--- src/components/UI/IconButton/IconButton.tsx
+++ src/components/UI/IconButton/IconButton.tsx
@@ -1,28 +1,28 @@
-import { FC, ComponentPropsWithoutRef} from 'react'
+import { FC, ComponentPropsWithoutRef } from 'react'
 import { FlattenSimpleInterpolation } from 'styled-components';
 
 import * as Styled from './IconButton.styled'
 
 interface IconButtonProps extends ComponentPropsWithoutRef<'button'> {
-  width?: number | string;
-  height?: number | string;
-  styles?: FlattenSimpleInterpolation;
+    width?: number | string;
+    height?: number | string;
+    styles?: FlattenSimpleInterpolation;
 }
 
 export const IconButton: FC<IconButtonProps> = ({
-  children,
-  width,
-  height,
-  onClick,
-  styles,
-  ...rest
+    children,
+    width,
+    height,
+    onClick,
+    styles,
+    ...rest
 }) =>
   <Styled.IconButton
-    width={width}
-    height={height}
-    onClick={onClick}
-    styles={styles}
-    {...rest}
+      width={width}
+      height={height}
+      onClick={onClick}
+      styles={styles}
+      {...rest}
   >
-    {children}
+      {children}
   </Styled.IconButton>
