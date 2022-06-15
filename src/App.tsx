6f35a296e89218faf7c240be4564114c909b87be
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2022-06-15 15:30:14 +0300

    delete BurgerMenu

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import styled, { css } from 'styled-components'
 import { Image } from './components/UI/Image/Image'
 import { Modal } from './components/UI/Modal/Modal'
 import catImage from './assets/catBig.webp'
-import { BurgerBtn } from './components/UI/buttons/BurgerBtn/BurgerBtn'
-import { MenuList } from './components/UI/MenuList/MenuList'
-import { MenuOverlay } from './components/UI/MenuOverlay/MenuOverlay'
 
 const items = ['Ботинки', 'Туфли', 'Рабочая обувь', 'Сандалии', 'Веган']
 
@@ -13,6 +10,7 @@ export const App = () => {
 
   return (
     <>
+   
     </>
   )
 }
