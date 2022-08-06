663eb2f0bf6ac7bb41eeee544cb4196f9a3b7b10
Anatolii Lukianenko <lukianenko.a.c@gmail.com>
2022-08-06 20:37:25 +0300

    update env

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,9 @@ const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
 
+console.log(process.env.REACT_APP_FIREBASE_API_KEY);
+
+
 root.render(
 	// <React.StrictMode>
 	<Provider store={store}>
